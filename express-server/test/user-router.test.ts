3c06358419b942c4b240417c2938ef789bce79e9
Hugo Phibbs <81545721+HugoPhibbs@users.noreply.github.com>
2023-08-24 13:32:30 +1200

    Deleting tests.
    No need. Are dependent on the AWS DB, and I ceebs

--- express-server/test/user-router.test.ts
+++
@@ -1,9 +0,0 @@
-const { checkUserIdMatchesUsername } = require("../src/express/user-router.ts");
-
-describe("Test dealing with photos", () => {
-    describe("Test posting photos", () => {});
-
-    describe("Test getting photos", () => {});
-});
-
-describe("Test updating users", () => {});
