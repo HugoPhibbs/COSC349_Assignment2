67db84fe3034719cbd03bb2d10867b2e23843621
Hugo Phibbs <81545721+HugoPhibbs@users.noreply.github.com>
2023-08-27 14:17:57 +1200

    Adding in more mock data

--- mysql-db/sql-scripts/2-insert.sql
+++ mysql-db/sql-scripts/2-insert.sql
@@ -6,9 +6,12 @@ INSERT INTO USER (firstName, lastName, isAdmin, email, password) VALUES
 
 -- Inserting data into the EVENT table
 INSERT INTO EVENT (title, location, startDate, endDate, description) VALUES
-('Company Picnic', 'Central Park', '2023-09-07 11:00:00', '2023-09-07 17:00:00', 'Annual company picnic for all employees.'),
-('Sales Conference', 'New York Marriott Marquis', '2023-09-08 09:00:00', '2023-09-08 11:00:00', 'Sales conference for all regional managers.'),
-('Product Launch', 'Jacob K. Javits Convention Center', '2024-09-05 05:00:00', '2024-09-05 07:00:00', 'Launch of new product line at the convention center.');
+('Company Picnic', 'Central Park', '2023-08-03 11:00:00', '2023-08-03 17:00:00', 'Annual company picnic for all employees.'),
+('Sales Conference', 'New York Marriott Marquis', '2023-08-06 09:00:00', '2023-08-07 11:00:00', 'Sales conference for all regional managers.'),
+('Product Launch', 'Jacob K. Javits Convention Center', '2023-09-05 05:00:00', '2024-09-05 07:00:00', 'Launch of new product line at the convention center.'),
+('Product Demo', 'The Auditorium', '2023-08-08 05:00:00', '2023-08-08 07:00:00', 'Demo for our new product line.'),
+('Team Lunch', 'The Office', '2023-08-10 11:00:00', '2023-08-10 12:30:00', 'Employee lunch at the French Cafe'),
+('Office Retreat', 'Lake Tekapo', '2023-08-24 05:00:00', '2023-08-28 07:00:00', 'Office retreat for all employees.');
 
 -- Inserting data into the ATTENDANCE_RECORD table
 INSERT INTO ATTENDANCE_RECORD (userId, eventId) VALUES
@@ -21,10 +24,10 @@ INSERT INTO ATTENDANCE_RECORD (userId, eventId) VALUES
 
 -- Inserting data into the EVENT_SCHEDULE_ITEM table
 INSERT INTO EVENT_SCHEDULE_ITEM (eventId, startDate, endDate, activity, description) VALUES
-(1, '2023-07-15 11:00:00', '2023-07-15 12:00:00', 'Lunch', 'Enjoy food and drinks with colleagues.'),
-(1, '2023-07-15 12:00:00', '2023-07-15 13:00:00', 'Games', 'Play games and participate in team-building activities.'),
-(2, '2023-09-12 09:00:00', '2023-09-12 10:30:00', 'Keynote Speech', 'Hear from the CEO about the company vision.'),
-(2, '2023-09-12 10:30:00', '2023-09-12 12:00:00', 'Breakout Sessions', 'Participate in workshops and networking sessions.'),
-(2, '2023-09-13 09:00:00', '2023-09-13 10:30:00', 'Keynote Speech', 'Hear from industry experts about the latest trends.'),
-(2, '2023-09-13 10:30:00', '2023-09-13 12:00:00', 'Breakout Sessions', 'Participate in workshops and networking sessions.'),
-(3, '2024-03-01 09:00:00', '2024-03-01 10:30:00', 'Product Demo', 'Get a firsthand look at the new product line.')
+(1, '2023-08-15 11:00:00', '2023-08-15 12:00:00', 'Lunch', 'Enjoy food and drinks with colleagues.'),
+(1, '2023-07-15 12:00:00', '2023-09-15 13:00:00', 'Games', 'Play games and participate in team-building activities.'),
+(2, '2023-08-12 09:00:00', '2023-09-12 10:30:00', 'Keynote Speech', 'Hear from the CEO about the company vision.'),
+(2, '2023-08-12 10:30:00', '2023-09-12 12:00:00', 'Breakout Sessions', 'Participate in workshops and networking sessions.'),
+(2, '2023-08-13 09:00:00', '2023-09-13 10:30:00', 'Keynote Speech', 'Hear from industry experts about the latest trends.'),
+(2, '2023-08-13 10:30:00', '2023-09-13 12:00:00', 'Breakout Sessions', 'Participate in workshops and networking sessions.'),
+(3, '2024-03-01 09:00:00', '2024-03-01 10:30:00', 'Product Demo', 'Get a firsthand look at the new product line.');
