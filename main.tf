7af50f7caea7204e5aae0ab0c8268349041d6b28
Hugo Phibbs <81545721+HugoPhibbs@users.noreply.github.com>
2023-09-25 20:36:11 +1300

    updating the tf file to deploy cron-job to aws

---
+++ main.tf
@@ -0,0 +1,50 @@
+provider "aws" {
+  region = "ap-southeast-2"
+  profile = "ecs-deploy"
+}
+
+resource "aws_ecr_repository" "cron-job" {
+  name = "cron-job"
+}
+
+resource "aws_ecs_task_definition" "cron-job-task" {
+  family                   = "cron-job-task"
+  container_definitions    = <<DEFINITION
+  [
+    {
+      "name": "cron-job-task",
+      "image": "${aws_ecr_repository.cron-job.repository_url}",
+      "essential": true,
+      "portMappings": [
+        {
+          "containerPort": 5000,
+          "hostPort": 5000
+        }
+      ],
+      "memory": 512,
+      "cpu": 256
+    }
+  ]
+  DEFINITION
+  requires_compatibilities = ["FARGATE"] # use Fargate as the launch type
+  network_mode             = "awsvpc"    # add the AWS VPN network mode as this is required for Fargate
+  memory                   = 512         # Specify the memory the container requires
+  cpu                      = 256         # Specify the CPU the container requires
+  execution_role_arn       = aws_iam_role.ecsTaskExecutionRole.arn
+}
+
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name               = "ecsTaskExecutionRole"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
