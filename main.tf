2aef04aecbf795f1751a011f9aa8864d3365b461
Hugo Phibbs <81545721+HugoPhibbs@users.noreply.github.com>
2023-09-26 18:06:57 +1300

    adding provisioning for rds db and ec2 instance on terraform file

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ provider "aws" {
   profile = "event-calendar"
 }
 
+// For CRON Job
 resource "aws_ecr_repository" "cron-job" {
   name = "cron-job"
 }
@@ -47,4 +48,53 @@ data "aws_iam_policy_document" "assume_role_policy" {
       identifiers = ["ecs-tasks.amazonaws.com"]
     }
   }
+}
+
+// For RDS DB
+resource "aws_db_instance" "event-calendar-db" {
+  allocated_storage = 20
+  storage_type = "gp2"
+  engine = "mysql"
+  engine_version = "5.7"
+  instance_class = "db.t2.micro"
+  username = "admin"
+  password = "password"
+  parameter_group_name = "default.mysql5.7"
+}
+
+// For React EC2 instance
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow inbound SSH traffic"
+
+  ingress {
+    description = "SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "react-app" {
+  ami = "ami-00f3471feb1f3897e" // Default AMI from AWS
+  instance_type = "t2.micro"
+  key_name = "event-calendar"
+
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
+#  provisioner "local-exec" {
+#    command = "ansible-playbook -i '${aws_instance.react-app.public_ip},' --private-key ~/.ssh/id_rsa -u admin playbook.yml"
+#  }
+}
+
+output "react-app-public-ip" {
+  value = aws_instance.react-app.public_ip
 }
