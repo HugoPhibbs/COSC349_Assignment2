e775ec5c46a47600c44b78a13e29fd05f4842c8a
Hugo Phibbs <81545721+HugoPhibbs@users.noreply.github.com>
2023-09-29 20:06:11 +1300

    scripts to provision the react and cron-job ec2s

---
+++ cron-job/provision_cron_job.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt-get update
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+source ~/.nvm/nvm.sh
+
+nvm install 16.13.0
+sudo apt-get install -y npm
+
+npm config set strict-ssl false
+npm install
+npm install -g ts-node
+npm run start
