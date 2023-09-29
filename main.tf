04e4086c47e59650ba7c9d207ab6bc4be0e3e380
Hugo Phibbs <81545721+HugoPhibbs@users.noreply.github.com>
2023-09-29 20:03:40 +1300

    finishing up terraform file.
    Setting secuurity policies

--- main.tf
+++ main.tf
@@ -3,69 +3,65 @@ provider "aws" {
   profile = "event-calendar"
 }
 
-// For CRON Job
-resource "aws_ecr_repository" "cron-job" {
-  name = "cron-job"
-}
-
-resource "aws_ecs_task_definition" "cron-job-task" {
-  family                   = "cron-job-task"
-  container_definitions    = <<DEFINITION
-  [
-    {
-      "name": "cron-job-task",
-      "image": "${aws_ecr_repository.cron-job.repository_url}",
-      "essential": true,
-      "portMappings": [
-        {
-          "containerPort": 5000,
-          "hostPort": 5000
-        }
-      ],
-      "memory": 512,
-      "cpu": 256
-    }
-  ]
-  DEFINITION
-  requires_compatibilities = ["FARGATE"] # use Fargate as the launch type
-  network_mode             = "awsvpc"    # add the AWS VPN network mode as this is required for Fargate
-  memory                   = 512         # Specify the memory the container requires
-  cpu                      = 256         # Specify the CPU the container requires
-  execution_role_arn       = aws_iam_role.ecsTaskExecutionRole.arn
-}
-
-resource "aws_iam_role" "ecsTaskExecutionRole" {
-  name               = "ecsTaskExecutionRole"
-  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
-}
-
-data "aws_iam_policy_document" "assume_role_policy" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["ecs-tasks.amazonaws.com"]
-    }
-  }
-}
-
 // For RDS DB
+variable "db_username" {}
+variable "db_password" {}
+
 resource "aws_db_instance" "event-calendar-db" {
   allocated_storage = 20
   storage_type = "gp2"
   engine = "mysql"
   engine_version = "5.7"
   instance_class = "db.t2.micro"
-  username = "admin"
-  password = "password"
+  username = var.db_username
+  password = var.db_password
   parameter_group_name = "default.mysql5.7"
+
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
 }
 
-// For React EC2 instance
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
+resource "aws_security_group"  "express_sg" {
+  name = "express_sg"
+
+  egress {
+    from_port = 0
+    to_port = 65535
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "rds_sg" {
+  name = "rds_sg"
+
+  ingress {
+    from_port = 3306
+    to_port = 3306
+    protocol = "tcp"
+    security_groups = [aws_security_group.express_sg.id, aws_security_group.ec2_sg.id]
+  }
+  depends_on = [aws_security_group.express_sg, aws_security_group.ec2_sg]
+}
+
+// For CRON Job
+resource "aws_instance" "cron-job" {
+  ami = "ami-00f3471feb1f3897e" // Default AMI from AWS
+  instance_type = "t2.micro"
+  key_name = "event-calendar"
+
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+
+  tags = {
+    Name = "cron-job"
+  }
+}
+
+resource "aws_security_group" "ec2_sg" {
   description = "Allow inbound SSH traffic"
+
+  tags = {
+    Name = "ec2_sg"
+  }
 
   ingress {
     description = "SSH from anywhere"
@@ -83,18 +79,28 @@ resource "aws_security_group" "allow_ssh" {
   }
 }
 
+// For React EC2 instance
+
 resource "aws_instance" "react-app" {
   ami = "ami-00f3471feb1f3897e" // Default AMI from AWS
   instance_type = "t2.micro"
   key_name = "event-calendar"
 
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
 
-#  provisioner "local-exec" {
-#    command = "ansible-playbook -i '${aws_instance.react-app.public_ip},' --private-key ~/.ssh/id_rsa -u admin playbook.yml"
-#  }
+  tags = {
+    Name = "react-app"
+  }
 }
 
-output "react-app-public-ip" {
-  value = aws_instance.react-app.public_ip
+output "react-app-public-dns" {
+  value = aws_instance.react-app.public_dns
+}
+
+output "cron-job-public-dns" {
+  value = aws_instance.cron-job.public_dns
+}
+
+output "db-endpoint" {
+  value = aws_db_instance.event-calendar-db.endpoint
 }
