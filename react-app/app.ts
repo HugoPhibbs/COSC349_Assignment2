09ea44aa7ca097ba8c7dee209563c4fc6bb125f4
Hugo Phibbs <81545721+HugoPhibbs@users.noreply.github.com>
2023-09-30 14:01:07 +1300

    fixing serving of static files in the react ec2 instance

--- react-app/app.ts
+++ react-app/app.ts
@@ -8,13 +8,9 @@ const PORT = 3001
 
 const apiTarget = process.env.API_HOST
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "public", 'index.html'));
-});
-
-app.use('/api', createProxyMiddleware({ target: apiTarget, changeOrigin: true, pathRewrite: { '^/api': '' } }));
+app.use('/', createProxyMiddleware({ target: apiTarget, changeOrigin: true, pathRewrite: { '^/api': '' } }));
 
 app.listen(PORT, () => {
     console.log(`Proxy server listening on port ${PORT}`)
