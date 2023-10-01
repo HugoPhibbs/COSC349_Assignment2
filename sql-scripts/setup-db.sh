ab53907662d918f27b8c592c4227f51c70281d06
Hugo Phibbs <81545721+HugoPhibbs@users.noreply.github.com>
2023-10-01 14:50:10 +1300

    Adding a DB host env variable into the db setup script

--- sql-scripts/setup-db.sh
+++ sql-scripts/setup-db.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# Change this on redeploys of the RDS DB
+export DB_HOST=terraform-20230930001142041200000005.cwbpnqbobcb0.ap-southeast-2.rds.amazonaws.com
+
+# MySQL is surprisingly hard to get via apt-get, so use MariaDB instead - its compatible with MySQL.
 sudo apt-get update
 sudo apt-get install mariadb-server -y
-mariadb -h terraform-20230928203833529700000001.cwbpnqbobcb0.ap-southeast-2.rds.amazonaws.com -u admin -p < ./schema.sql
-mariadb -h terraform-20230928203833529700000001.cwbpnqbobcb0.ap-southeast-2.rds.amazonaws.com -u admin -p < ./insert.sql
+mariadb -h $DB_HOST -u admin -p < ./schema.sql
+mariadb -h $DB_HOST -u admin -p < ./insert.sql
 
 sudo systemctl stop mariadb -y
 sudo apt-get purge --auto-remove mariadb-server mariadb-client -y
